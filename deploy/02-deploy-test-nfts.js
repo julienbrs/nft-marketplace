4c85bd9937bfaca6c105bcccf12641fca177ed60
Julien Bourseau <Julien.Bourseau@grenoble-inp.org>
2022-12-22 22:42:10 +0100

    deploy scripts

---
+++ deploy/02-deploy-test-nfts.js
@@ -0,0 +1,26 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    let args = []
+    const testNft = await deploy("TestNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying the contract...")
+        await verify(testNft.address, args)
+    }
+
+    log(" Contract verified")
+    log("============================")
+}
+
+module.exports.tag = ["all", "testNft"]
---
+++ test/unit/NftMarketPlace.test.js
@@ -0,0 +1,7 @@
+const { assert, expect } = require("chai")
+
+const { network, deployments, ethers } = require("hardhat")
+
+const { developmentChains } = require("../../helper-hardhat-config")
+
+// TODO do all unit tests
